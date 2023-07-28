7ca0ae73a9e578815edcb521df84723dfb4c0ac6
Danil Egorkin <93032618+elmore17@users.noreply.github.com>
2023-07-28 21:52:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -73,3 +73,6 @@ def index():
             sentiment = "Отрицательный комментарий."
         messageSentiment = sentiment
     return render_template('index.html', messageScore=messageScore,messageSentiment = messageSentiment, comment = comment)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=int("3000"),debug=True)
